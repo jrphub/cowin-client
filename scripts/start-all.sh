928f11a397bb9d5b6a18902beb6b6553b7492ee1
jrphub <jyotiranjanpattnaik@gmail.com>
2021-05-30 16:07:54 +0530

    start-all script added

---
+++ scripts/start-all.sh
@@ -0,0 +1,7 @@
+sh ../run-bslots.sh > /dev/null 2>&1 &
+sh ../run-cslots18.sh > /dev/null 2>&1 &
+sh ../run-cslots45.sh > /dev/null 2>&1 &
+sh ../run-hslots.sh > /dev/null 2>&1 &
+sh ../run-kslots18.sh > /dev/null 2>&1 &
+sh ../run-kslots45.sh > /dev/null 2>&1 &
+sh ../run-mslots.sh > /dev/null 2>&1 &
